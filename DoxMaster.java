0be5d4150d1d9a7185a3d99c60f32f1bff34ea21
323 <starcraft47@gmail.com>
2014-04-12 01:01:26 -0400

    Changed HTTPS support to a method
    Instead of using a separate class, I decided to add in HTTPS support as
    a method instead.

--- DoxMaster.java
+++ DoxMaster.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.*;
+import java.security.*;
+
 class DoxMaster {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String readme0 = "Welcome to Dox Master.";
         String readme1 = "Stop trying to decompile my program.";
         String readme2 = "Coded by weebs.";
         HTTPRequest request = new HTTPRequest();
         Scanner name = new Scanner(System.in);
+        String url; //String for storing URLs
         String source; //String for storing page sources
         String targetName; //Store target's name
         String formattedTargetName; //Store the URL-formatted target name
@@ -20,8 +26,45 @@ class DoxMaster {
         System.out.println("Doxing your target...");
         System.out.println("Checking Facebooks with the name " + targetName);
         formattedTargetName = targetName.replace( ' ', '+' );
-        request.setURL("http://www.facebook.com/search.php?q=" + formattedTargetName);
-        source = request.get(); //Grab the results
+        url = "https://www.facebook.com/search.php?q=" + formattedTargetName;
+        source = SSLSocketClient(url);
         System.out.println(source);
+    }
+    public static String SSLSocketClient(String parameters) throws Exception {
+        String endhtml = "</html>";
+        Boolean found;
+        String urlString = parameters;
+        URL url = new URL(urlString);
+    
+        Security.addProvider(
+          new com.sun.net.ssl.internal.ssl.Provider());
+    
+        SSLSocketFactory factory = 
+          (SSLSocketFactory)SSLSocketFactory.getDefault();
+        SSLSocket socket = 
+          (SSLSocket)factory.createSocket(url.getHost(), 443);
+    
+        PrintWriter out = new PrintWriter(
+            new OutputStreamWriter(
+              socket.getOutputStream()));
+        out.println("GET " + urlString + " HTTP/1.1");
+        out.println();
+        out.flush();
+    
+        BufferedReader in = new BufferedReader(
+          new InputStreamReader(
+          socket.getInputStream()));
+    
+        String line;
+        String source = "";
+        
+        while (((line = in.readLine()) != null) && (found = false)) {
+              source += line;
+              found = source.contains(endhtml);
+        }
+        //System.out.println(source); //For testing
+        out.close();
+        in.close();
+        return source;
     }
 }
--- HTTPRequest.java
+++ HTTPRequest.java
@@ -158,34 +158,6 @@ public class HTTPRequest {
                 }
         }
         
-        public String gets(String arg) throws Exception {
-                String urlString = (args.length == 1) ? 
-                    args[0] : "http://www.verisign.com/index.html";
-                URL url = new URL(urlString);
-            
-                Security.addProvider(
-                    new com.sun.net.ssl.internal.ssl.Provider());
-            
-                SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-                SSLSocket socket = (SSLSocket)factory.createSocket(url.getHost(), 443);
-            
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-                out.println("GET " + urlString + " HTTP/1.1");
-                out.println();
-                out.flush();
-            
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            
-                String line;
-            
-                while ((line = in.readLine()) != null) {
-                  System.out.println(line);
-                }
-            
-                out.close();
-                in.close();
-        }
-        
         public String post(String link, String parameters) {
                 this.setURL(link);
                 return this.post(parameters);
